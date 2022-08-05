387e2eef07bf3223dff86ea5f79f6a113e12ffa8
Leandro Santana <62582790+leld21@users.noreply.github.com>
2022-08-05 11:12:25 -0300

    Update hellofor.s

--- hellofor.s
+++ hellofor.s
@@ -8,19 +8,19 @@
 .global main
 main:
 	push    rbp
-    mov     rbp, rsp
-    sub rsp, 16
+	mov     rbp, rsp
+	sub rsp, 16
 
-    mov DWORD PTR [rbp-4], 5
-    mov DWORD PTR [rbp-8], 0
-    cmp DWORD PTR [rbp-8], 5
-    jmp .cont
+	mov DWORD PTR [rbp-4], 5
+	mov DWORD PTR [rbp-8], 0
+	cmp DWORD PTR [rbp-8], 5
+	jmp .cont
 .for:
 	add	DWORD PTR [rbp-4], 1
-    add	DWORD PTR [rbp-8], 1
+	add	DWORD PTR [rbp-8], 1
 .cont:
 	cmp DWORD PTR [rbp-8], 5
-    jne .for
+	jne .for
 	mov rax, 0
 	lea rdi, [rip+parameter]
 	mov rsi, [rbp-4]
