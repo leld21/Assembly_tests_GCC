e815b8edaecf2723e6172456f8de0c83dcf171e5
Leandro Santana <62582790+leld21@users.noreply.github.com>
2022-08-08 13:07:50 -0300

    multiplicador de vetor usando ponteiro

--- vetor_mult.s
+++ vetor_mult.s
@@ -11,24 +11,48 @@ main:
     mov     rbp, rsp
     sub rsp, 16
 
-    mov DWORD PTR [rbp-4], 3
     mov rdi, 3 * 4
 	call malloc@plt
+
 	mov [rip + vector], rax
-	jmp .cont
-.for:
-	add	DWORD PTR [rbp-4], 1
-    add	DWORD PTR [rbp-8], 1
-.cont:
-	cmp DWORD PTR [rbp-8], 3
-    jne .for
+.set1:
+	mov rcx, 0
+.readfor:
+	cmp rcx, 5
+    je .set2
+    mov [rbp - 4], rcx
+    // pra multiplicar precisa primeiro colocar o valor do //multiplicando no EAX
+    mov eax, 2
+    mul rcx
+    mov rdx, [rip + vector]
+    mov [rdx + rcx * 4], eax
+    inc	rcx
+    jmp .readfor
+.set2:
+	mov rcx, 0
+.printfor:
+	cmp rcx, 5
+	je .cont
+
+	mov [rbp - 8], rcx
+
 	mov rax, 0
-	lea rdi, [rip+parameter]
-	mov rsi, [rbp-4]
-	mov rdx, '!'
+	lea rdi, [rip + parameter]
+	mov rsi, [rip + vector]
+	mov rsi, [rsi + rcx * 4]
 	call printf@plt
+
+	mov rcx,[rbp - 8]
+	inc	rcx
+	jmp .printfor
+.cont:
+    //free array
+	mov rdi, [rip + vector]
+	call free@plt
+	// free stack and finish
 	xor rax, rax
-	leave
+	mov rsp, rbp
+	pop rbp
 	ret
 .section .bss
 vector:
