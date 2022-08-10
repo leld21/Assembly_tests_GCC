f359241270f1504e3d3f565c12b8589fe1fa1d02
leld21 <leandro7531591@gmail.com>
2022-08-10 00:19:31 -0300

    versao em c concluida

--- fopen.c
+++ fopen.c
@@ -1,74 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-/*
-int* ordena (int a*[],int n){
+
+void bubblesort(int *vetor, int n){
+int aux;
+for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        if(vetor[i]>vetor[j]){
+            aux=vetor[i];
+            vetor[i]=vetor[j];
+            vetor[j]=aux;
+        }
+    }
+}
 
 }
-*/
 
 int main(int argc, char **argv[]){
 
 FILE * filePointer; 
 filePointer = fopen("entrada.txt", "r");
 
-int numero;
-fscanf(filePointer, "%i", &numero);
-    
-    for(int i=0;i<numero;i++){
-        int n;
-        char valores[30];
-        fscanf(filePointer, "%i", &n);
-        int *vetor;
-        vetor=malloc(sizeof(int)*n);
-        fgets(valores,sizeof(valores), filePointer);
-        for(int j=0;j<(n*2)-1;j++){
-            if(j%2==0){
-                vetor[(j/2)]=((int)valores[j])-'0';
-            }
-        }
-        for(int i=0;i<n;i++){
-            printf("%d\n",vetor[i]);
-        }
-    }
-
-/*
-char x[36];
-fgets(x, sizeof(x), filePointer);
-int numero=atoi(x);
-char y[10];
-
-for(int i=0;i<numero;i++){
-    fgets(y, sizeof(y), filePointer);
-    int n=atoi(y);
+int quant;
+fscanf(filePointer, "%i", &quant);
+for(int i=0;i<quant;i++){
+    int n;
     int *vetor;
-    vetor = malloc(sizeof(int)*n);
-    char elementos[50];
-    fgets(elementos, sizeof(elementos), filePointer);
-    printf("%s\n",elementos);
-}
-*/
-/*
-char y[10];
-fgets(y, sizeof(y), filePointer);
-printf("%c\n",y[0]);
-char z[10];
-fgets(y, sizeof(y), filePointer);
-printf("%i\n",atoi(y));
-*/
-/*
-while (fgets(line, sizeof line, file) != NULL) 
-{
-    if (count == lineNumber)
-    {
-        //use line or in a function return it
-        //in case of a return first close the file with "fclose(file);"
+    fscanf(filePointer, "%i", &n);
+    vetor=malloc(sizeof(int)*n);
+    printf("[%d] ",i);
+    for(int j=0;j<n;j++){
+        fscanf(filePointer,"%i",&vetor[j]);
     }
-    else
-    {
-        count++;
-    }
+    bubblesort(vetor,n);
+    for(int j=0;j<n;j++){
+        if(j==n-1){
+            printf("%d\n",vetor[j]);
+        }else{
+            printf("%d ",vetor[j]);
+        }
+    }     
 }
-*/
 fclose(filePointer);
 
 }
